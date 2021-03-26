1e9c7476d2bda287c3050218c7d955c98f1f5ceb
Stefano <tepo@hotmail.it>
2021-03-26 16:59:50 +0100

    Passo 3: possibile aggiungere nuovi elementi alla lista da input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+/*- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista */
+
 var list = new Vue({
   el: "#list",
   data: {
     toDo: ["Fare la spesa", "Comprare il giornale", "Giocare a FIFA", "Dormire"],
+    newItem: "",
   },
   methods: {
-    
+    deleteItem: function(index) {
+      this.toDo.splice(index, 1);
+    },
+    addItem: function() {
+      if (this.newItem != "") {
+        this.toDo.push(this.newItem);
+        this.newItem = "";
+      }
+    }
   }
 });
